2952845d2025988984287ba318a3941ced1673b8
peposlawa <peposlawa@gmail.com>
2024-09-11 22:29:26 +0100

    weather data displayed on submit

--- weather-app/components/footer/footer.tsx
+++ weather-app/components/footer/footer.tsx
@@ -1,4 +1,5 @@
 import styles from './footer.module.css'
+import Image from 'next/image';
 
 export default function Footer() {
     return (
@@ -7,6 +8,7 @@ export default function Footer() {
                 <div className={styles.footerText}>
                     Brought to you by © Breakfast BAP
                 </div>
+                <Image src="/burger.png" alt="Bap" width={20} height={20} className={styles.footerBap}/>
             </footer>
         </>
     )
--- weather-app/components/main/main.tsx
+++ weather-app/components/main/main.tsx
@@ -1,22 +1,35 @@
 'use client'
 import { useState } from "react";
 import styles from "./main.module.css";
+import Image from 'next/image';
 
 export default function WeatherApp() {
 
     const [city, setCity] = useState<string>('');
+    const [weatherData, setWeatherData] = useState<any>(null);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         try {
-            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=cb61364190d9fb85a02a68fc9973417c`);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cb61364190d9fb85a02a68fc9973417c&units=metric`);
             const data = await response.json();
-            console.log(data);
+            setWeatherData(data);
         } catch (error) {
             console.error('Error fetching weather data:', error);
         }
-    }
+    };
+
+    const currentDate = new Date();
+    const formattedDate = `
+    ${currentDate.toLocaleDateString('en-UK', {
+        weekday: 'short'
+    })}, 
+    ${currentDate.toLocaleDateString('en-UK', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+    })}`;
 
     return (
         <>
@@ -31,6 +44,23 @@ export default function WeatherApp() {
                     type="submit"
                     className={styles.appButton}>Submit</button>
             </form>
+
+            {weatherData && (
+                <div className={styles.appWeather}>
+                    <p className={styles.weatherDate}>{formattedDate}</p>
+                    <p className={styles.weatherCity}>{weatherData.name}, {weatherData.sys.country}</p>
+                    <p className={styles.weatherTemp}>
+                        <Image src="/thermometer.png" alt="Temp" width={40} height={40} />
+                        {Math.round(weatherData.main.temp)}°C</p>
+                    <p className={styles.weatherDesc}>Conditions: {weatherData.weather[0].description}</p>
+                    <p className={styles.weatherTempLike}>
+                        Feels like: {Math.round(weatherData.main.feels_like)}°C</p>
+                    <p className={styles.weatherMinMax}>
+                        Min: {Math.round(weatherData.main.temp_min)}°C |
+                        Max: {Math.round(weatherData.main.temp_max)}°C</p>
+                </div>
+            )
+            }
         </>
     )
 }
