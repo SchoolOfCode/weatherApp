097c25ab15f37635b82cd34a3817076a5b69de63
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-09-11 14:26:48 +0100

    add functionality that fetches for data from weather api upon submitting the form

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from 'next/image';
 
+
+
+
+
+// useEffect(() => {
+
+// }, [city]);
+
+
+
 export default function Home() {
+
+  const [city, setCity] = useState('');
+
+
+   // Function to handle form submission
+   const handleSubmit = async (event : React.FormEvent) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    try {
+      // Make a fetch request to the weather API with the city input
+      const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=cb61364190d9fb85a02a68fc9973417c`);
+      const data = await response.json(); // Parse the JSON response
+      console.log(data); // Log the data to the console
+    } catch (error) {
+      console.error('Error fetching weather data:', error); // Log any errors
+    }
+  };
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -16,8 +46,8 @@ export default function Home() {
         {/* Main Section */}
         <main>
 
-          <form className={styles.appSearch}>
-            <input type="text" placeholder="Search city..." name="search" className={styles.inputWindow} />
+          <form className={styles.appSearch} onSubmit= {handleSubmit}>
+            <input type="text" placeholder="Search city..." name="search" className={styles.inputWindow} onChange={(e) => setCity(e.target.value)}/>
             <button type="submit" className={styles.appButton}>Submit</button>
           </form>
 
